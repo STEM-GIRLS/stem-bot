6695126560d0b2c88b4c0161ca89621b00a9c0c8
Carol Gomides <anacarolinagteixeira16@gmail.com>
2025-07-20 16:04:16 -0300

    Implementação Inicial dos comandos de entrada e saída do servidor

---
+++ main.py
@@ -0,0 +1,29 @@
+import discord 
+from discord.ext import commands
+
+intents = discord.Intents.all()
+
+bot = commands.Bot("/", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot ligado")
+
+#mensagem de boas-vindas
+@bot.event
+async def on_member_join(member):
+    channel_id = 1390807035636875427
+    channel = bot.get_channel(channel_id)
+    if channel:
+        await channel.send(f"Bem-vinda, {member.mention}! 🌷")
+
+#mensagem de saída
+@bot.event
+async def on_member_remove(member):
+
+    channel_id = 1393722105883004948
+    channel = bot.get_channel(channel_id)
+    if channel: 
+        await channel.send(f"👋 {member.mention} saiu do servidor!")
+
+bot.run("#adicionar o token do bot")
