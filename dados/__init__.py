2588f69e9fb28cabab42b6ae559632b8c2cf90ab
Bianca Rodrigues <85198342+namikki@users.noreply.github.com>
2025-07-23 17:37:27 -0300

    Revert "Módulo de Eventos e Melhorias Gerais no Bot"

--- dados/__init__.py
+++
@@ -1,1 +0,0 @@
-# Arquivo __init__.py para permitir importações da pasta dados 
--- services/__init__.py
+++
@@ -1,2 +0,0 @@
-# Services - Módulos de serviço para o bot
-# Esta pasta contém serviços que podem ser usados pelos Cogs 
